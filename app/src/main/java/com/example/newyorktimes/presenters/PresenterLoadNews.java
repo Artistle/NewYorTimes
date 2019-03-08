5c19ff23c34c11930beeda544b999c5750c5f902
Artistle <nikitaobryvkov39@gmail.com>
2019-03-08 05:44:15 -0500

    add retrofits and rv,cv

---
+++ app/src/main/java/com/example/newyorktimes/presenters/PresenterLoadNews.java
@@ -0,0 +1,4 @@
+package com.example.newyorktimes.presenters;
+
+public class PresenterLoadNews {
+}
---
+++ app/src/main/java/com/example/newyorktimes/view/View.java
@@ -0,0 +1,7 @@
+package com.example.newyorktimes.view;
+
+import com.arellomobile.mvp.MvpView;
+
+public interface viewNews extends MvpView {
+    public void adapter(RecyclerViewNewsAdapter adapter);
+}
