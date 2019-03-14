60b293c1024f3b3b1a5dc7b91edc9c0b2beba132
Artistle <nikitaobryvkov39@gmail.com>
2019-03-14 13:41:36 -0400

    add fragments

---
+++ app/src/main/java/com/example/newyorktimes/view/Fragments/FragmentFix.java
@@ -0,0 +1,4 @@
+package com.example.newyorktimes.view.Fragments;
+
+public class FragmentFix {
+}
---
+++ app/src/main/java/com/example/newyorktimes/view/Fragments/NewsFragment.java
@@ -0,0 +1,26 @@
+package com.example.newyorktimes.view;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.newyorktimes.R;
+
+public class NewsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_recycler_news, container, false);
+    }
+
+    public NewsFragment() {
+    }
+
+    public static  NewsFragment getInstance(){
+        return new NewsFragment();
+    }
+}
